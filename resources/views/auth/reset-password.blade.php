95e7858dcb195d1054e6b74856ba40d98b4a8348
Precious2804 <anipreciousebuka@gmail.com>
2021-10-23 18:32:48 +0100

    implemented forgot/reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -13,61 +13,47 @@
         <!--============= Sign In Section Starts Here =============-->
         <div class="account-wrapper">
             <div class="account-body">
-                <h4 class="title mb-20">Welcome To {{env('APP_NAME')}}</h4>
-                <span class="d-block mb-30 mt-2">Sign in with your work email</span>
-                @if(Session::get('reg'))
-                <div class="alert alert-success">
-                    {{Session::get('reg')}}
-                </div>
-                @endif
-                @if(Session::get('infoEmail'))
-                <div class="alert alert-danger">
-                    {{Session::get('infoEmail')}}
-                </div>
-                @endif
-                @if(Session::get('info'))
-                <div class="alert alert-danger">
-                    {{Session::get('info')}}
-                </div>
-                @endif
-                @if(Session::get('unverified'))
-                <div class="alert alert-danger">
-                    {{Session::get('unverified')}}
-                </div>
-                @endif
-                @if(Session::get('out'))
-                <div class="alert alert-success">
-                    {{Session::get('out')}}
-                </div>
-                @endif
+                <h4 class="title mb-20">Update your Password now</h4>
                 @if(Session::get('status'))
                 <div class="alert alert-success">
                     {{Session::get('status')}}
                 </div>
                 @endif
-                <form class="account-form" method="POST" action="{{route('do-login')}}">
+                @if(Session::get('email'))
+                <div class="alert alert-danger">
+                    {{Session::get('email')}}
+                </div>
+                @endif
+                @if(Session::get('invalid'))
+                <div class="alert alert-danger">
+                    {{Session::get('invalid')}}
+                </div>
+                @endif
+                <form class="account-form" method="POST" action="{{route('password.update')}}">
                     @csrf
+                    <input type="hidden" name="token" value="{{$token}}">
                     <div class="form-group">
                         <label for="sign-up">Your Email </label>
-                        <input type="text" placeholder="Enter Your Email " id="sign-up" name="email">
+                        <input type="hidden" placeholder="Enter Your Email " id="sign-up" name="email" value="{{$email}}">
+                        <input type="email" placeholder="Enter Your Email " id="sign-up" name="email" value="{{$email}}" disabled>
                         <span class="text-danger"> @error('email') {{$message}} @enderror </span>
                     </div>
                     <div class="form-group">
-                        <label for="pass">Password</label>
+                        <label for="pass">New Password</label>
                         <input type="password" placeholder="Enter Your Password" id="pass" name="password">
-                        <span class="sign-in-recovery">Forgot your password? <a href="{{route('password.request')}}">recover password</a></span>
                         <span class="text-danger"> @error('password') {{$message}} @enderror </span>
                     </div>
+                    <div class="form-group">
+                        <label for="sign-up">Confirm new Password </label>
+                        <input type="password" name="password_confirmation" placeholder="Confirm your Password ">
+                        <span class="text-danger"> @error('password_confirmation') {{$message}} @enderror </span>
+                    </div>
                     <div class="form-group text-center">
-                        <button type="submit" class="mt-2 mb-2">Sign In</button>
+                        <button type="submit" class="mt-2 mb-2">Update Password Now</button>
                     </div>
                 </form>
             </div>
-            <div class="or">
-                <span>OR</span>
-            </div>
             <div class="account-header pb-0">
-                <a href="#0" class="sign-in-with"><img src="assets/images/icon/google.png" alt="icon"><span>Sign In with Google</span></a>
                 <span class="d-block mt-15">Don't have an account? <a href="{{route('register')}}">Sign Up Here</a></span>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +30,62 @@ Route::get('auth.login', [MainController::class, 'login'])->name('login');
 Route::get('auth.register', [MainController::class, 'register'])->name('register');
 Route::post('/do-register', [MainController::class, 'doRegister'])->name('do-register');
 Route::post('/do-login', [MainController::class, 'doLogin'])->name('do-login');
+
+
+
+Route::get('/password.request', function () {
+    return view('auth.forgot');
+})->name('password.request');
+
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate(['email' => 'required|email']);
+    $email = $request->email;
+    $checkEmail = User::where('email', $email)->first();
+    if (!$checkEmail) {
+        return back()->with('invalid', "This email address is not Registered on this Platform");
+    } else {
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    }
+})->name('password.email');
+
+Route::get('auth.reset-password/{token}', function (Request $request, $token) {
+    $bnn = ['token' => $token];
+    $email = ['email'=> $request->email];
+    return view('auth.reset-password')->with($bnn)->with($email);
+})->name('password.reset');
+
+Route::post('/reset-password', function (Request $request) {
+    $request->validate([
+        'token' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:8|confirmed',
+    ]);
+    $checkEmail = User::where('email', $request->email)->first();
+    if (!$checkEmail) {
+        return back()->with('invalid', "Invalid Email Address");
+    } else {
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                    // 'password' => $password
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
+    }
+})->name('password.update');
+
